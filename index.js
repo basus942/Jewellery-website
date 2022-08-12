e8e936a16f00f5bd49e6423b2c5addf2ae7fdb02
Subhajit Basu <basus942@gmail.com>
2022-08-12 23:09:36 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,19 @@
+const express=require("express");
+const app=express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+    res.render("home")
+})
+
+app.get("/aboutus",(req,res)=>{
+res.render("aboutus")
+})
+app.get("/contact",(req,res)=>{
+    res.render("contact")
+    })
+
+app.listen(3000||process.env.PORT,()=>{
+    console.log("Server is running on http://localhost:3000");
+})
